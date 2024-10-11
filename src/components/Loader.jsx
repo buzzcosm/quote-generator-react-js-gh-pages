6184f2e3308bffea83498a632198f3d5cb40a1fd
Evgueni Kounik <kounik.ch@gmail.com>
2024-10-11 19:40:42 +0200

    Add loader component and change icon

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import { RotatingLines } from "react-loader-spinner";
+import PropTypes from 'prop-types';
+
+function Loader(props) {
+  return (
+    <RotatingLines
+      strokeColor="grey"
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="96"
+      visible={props.visible}
+    />
+  )
+}
+
+Loader.propTypes = {
+  visible: PropTypes.bool.isRequired,
+};
+
+export default Loader;
